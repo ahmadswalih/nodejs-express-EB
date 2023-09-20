6fd950d9db5511aeb843a80a97f427402d62dbc7
ahmadswalih <ahmadswalih777@gmail.com>
2023-09-20 11:10:43 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Helllo Worldddd");
+});
+
+app.listen(port, () => {
+  console.log(`Application is Running in the PORT ${port}`);
+});
